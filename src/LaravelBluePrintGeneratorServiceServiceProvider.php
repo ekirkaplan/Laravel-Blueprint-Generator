f4c14239cd7efa97d10d68ceb83cf1aa3a1c0477
Egemen KIRKAPLAN <atamyazilim@gmail.com>
2023-03-14 12:00:07 +0300

    First Commit

---
+++ src/LaravelBluePrintGeneratorServiceServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ekirkaplan\LaravelBluePrint;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelBluePrintGeneratorServiceServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
